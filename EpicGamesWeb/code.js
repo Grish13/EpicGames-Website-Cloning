713d1cb9d418e5341aec6789f7c9b8b09fbc8cf2
Nxn Benedicto <53139745+Grish13@users.noreply.github.com>
2022-09-14 20:13:34 +0800

    Add files via upload
    40% done, not fully responsive

---
+++ EpicGamesWeb/code.js
@@ -0,0 +1,28 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    controls: false,
+    navContainer:'.rightCarouCon',
+    navAsThumbnails: true,
+
+
+
+
+
+  });
+
+  var slider = tns({
+      container: '.sale-slider',
+      items: 5,
+      slideBy: 'page',
+      autoplay: true,
+      gutter:22,
+      nav:false,
+      autoplayButtonOutput: false,
+      controlsContainer: "#controls",
+      prevButton: ".previous",
+      nextButton: ".next"
+    });
